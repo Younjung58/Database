eb5463f5c24103cc5c1ae9f24dad3c7408b62544
Younjung58 <skykh999@naver.com>
2024-09-03 10:49:47 +0900

    내용추가

--- 9.2_test.sql
+++ 9.2_test.sql
@@ -19,11 +19,17 @@ insert into users values (10,'kimmi','incheon',79.92,1,'810505-1');
 insert into users values (11,'ryusu','seoul',85.32,4,'861205-2');
 insert into users values (12,'Gosu',null,82.13,4,'810715-1');
 
+-- 설명: 2 5 6 8 9 10 11 15 16
+
 --1. 모든 사람들의 이름과 점수와 생년월일을 검색하시오.  
 select name, point, jumin from users;
 
 --2. 80점 이상의 사람의 이름과 주소, 점수를 검색하시오.
 select name, addr, point from users where point>=80;
+--2번 왜이렇게 했는가?
+--참조해야할 대상 테이블은 users(from)이고, 
+--참과 거짓으로 튜플을 나누어 출력할 부분인가를 결정할 수 있는 조건은 점수가 80점 이상인가(where)이며,
+--최종 출력하고자 하는 부분은 이름(name),주소(addr), 점수(point)이다.(select)
 
 --3. 이름이 kim으로 시작되는 사람의 이름과 주소와 점수를 검색하시오.
 select name, addr, point from users where name like 'kim%';
@@ -33,9 +39,18 @@ select point+10 from users;
 
 --5. 1학년의 점수를 +1점씩 올려서 출력하세요. (단, 원본 데이터에 반영되지 않습니다.)
 select point+1 from users where grade=1;
+--5번 왜이렇게 했는가?
+--참조해야할 대상 테이블은 users(from)이고, 
+--참과 거짓으로 튜플을 나누어 출력할 부분인가를 결정할 수 있는 조건은 학년이 1학년인가(where)이며,
+--최종 출력하고자 하는 부분은 점수를 1점씩 올린 것(point+1)이다.(select)
 
 --6. B등급을 획득한 사람의 이름, 주소, 점수를 출력하세요 (B등급 80점 이상 90점 미만입니다)
 select name, addr, point from users where point>=80 and point<90;
+--6번 왜이렇게 했는가?
+--참조해야할 대상 테이블은 users(from)이고, 
+--참과 거짓으로 튜플을 나누어 출력할 부분인가를 결정할 수 있는 조건은 점수가 B등급인가,
+--즉 점수가 80점이상 90점미만인가(where)이며,
+--최종 출력하고자 하는 부분은 이름(name),주소(addr), 점수(point)이다.(select)
 
 --7. 쿼리 결과 중 NULL은 실제 입력하지 않은 값입니다.
 --   주소를 입력하지 않은 학생의 이름과, 주소, 학년, 점수, 주민번호를 출력하세요
@@ -43,13 +58,29 @@ select name, addr, grade, point, jumin from users where addr is null;
 
 --8. 4학년의 점수를 10%로 올린 점수를 계산하세요.(단, 원본데이터는 반영되지 않는다)
 select point*1.1 from users where grade=4;
+--8번 왜이렇게 했는가?
+--참조해야할 대상 테이블은 users(from)이고, 
+--참과 거짓으로 튜플을 나누어 출력할 부분인가를 결정할 수 있는 조건은 학년이 4학년인가(where)이며,
+--최종 출력하고자 하는 부분은 점수를 10%올린 점수(point*1.1)이다.(select)
 
 --9. 점수가 낮은 학생부터 출력하세요.  번호, 이름, 주소, 포인트
 select no, name, addr, point from users order by point asc;
+--9번 왜이렇게 했는가?
+--참조해야할 대상 테이블은 users(from)이고, 
+--참과 거짓으로 튜플을 나누어 출력할 부분인가를 결정할 수 있는 조건은 없으며(where절 없음),
+--최종 출력하고자 하는 부분은 번호(no), 이름(name), 주소(add), 포인트(point)이다.(select)
+--또한 이를 출력하고 하는 튜플의 순서(정렬)은 점수가 낮은순서부터이다.(점수의 오름차순,point asc)(order by)
 
 --10. 학년을 오름차순으로 정렬하시오, 단 학년이 동일 할 경우 포인트가 높은 사람이 먼저 출력됩니다.
 --    출력은 번호, 이름, 학년, 포인트
 select no, name, grade, point from users order by grade asc, point desc;
+--10번 왜이렇게 했는가?
+--참조해야할 대상 테이블은 users(from)이고, 
+--참과 거짓으로 튜플을 나누어 출력할 부분인가를 결정할 수 있는 조건은 없으며(where절 없음),
+--최종 출력하고자 하는 부분은 번호(no), 이름(name), 학년(grade), 포인트(point)이다.(select)
+--또한 이를 출력하고 하는 튜플의 순서(정렬)은 학년의 오름차순이며(grade asc-정렬1조건), 
+--학년 동일경우 포인트의 내림차순으로 다시 정렬되어 나타낸다.(point desc-정렬2조건)
+--이 두가지의 정렬을 같이 나타내는 방식은 , 로 연결하는 것이다(order by 정렬1조건, 정렬2조건)
 
 --11. 현재의 점수에서 -10을 보정한 결과 80점 이상인 사람의 이름과 점수, 보정한 결과 점수를 출력하세요.?
 select name, point, point-10 보정한결과 from users where point-10>=80;
@@ -66,8 +97,23 @@ select count(*) from users;
 select count(*) from users where grade=1;
 --15. 모든 학생의 이름과 학년을 출력하시오. 단 이름은 모두 소문자로만 출력하시오. 
 select lower(name), grade from users;
+--15번 왜이렇게 했는가?
+--참조해야할 대상 테이블은 users(from)이고, 
+--참과 거짓으로 튜플을 나누어 출력할 부분인가를 결정할 수 있는 조건은 없으며(where절 없음),
+--최종 출력하고자 하는 부분은 이름(name), 학년(grade)이다.(select)
+--단, 이름은 출력할 때(select) 소문자로만 출력해야 하는데, 이는 수식으로 불가능하다.
+--이를 가능하게 해주는 함수는 lower(name) 이다.
+
 --16. 2학년 학생의 이름과 학년, 주소를 출력하시오. 보안을 위해서 주소는 앞글자 2글자만 출력하시오.
 select name, grade, substr(addr,1,2) 주소 from users where grade=2;
+--16번 왜이렇게 했는가?
+--참조해야할 대상 테이블은 users(from)이고, 
+--참과 거짓으로 튜플을 나누어 출력할 부분인가를 결정할 수 있는 조건은 학년이 2학년인가(grade=2)(where절),
+--최종 출력하고자 하는 부분은 이름(name), 학년(grade), 주소(addr)이다.(select)
+--단, 주소를 출력할 때(select) 앞 2글자만 출력해야 하는데, 이는 수식으로 불가능하다.
+--이를 가능하게 해주는 함수는 substr이며, 매개변수가 3개이고 사용법은 substr(대상,시작위치,추출갯수)이므로 
+--적용하면 substr(addr,1,2)이다.
+
 --17. 모든 학생의 점수의 일의 자리는 절삭하시오. - 반올림 없음 (단, 원본 데이터에 반영되지 않습니다.)
 select trunc(point,-1) from users;
 --18. 모든 학생의 점수를 반올림하여 출력하시오. (단, 원본 데이터에 반영되지 않습니다.)
@@ -91,7 +137,7 @@ select concat(substr(addr,1,2),'*') 보안주소 from users;
 --25. 이름의 맨 앞에 *를 맨 뒤에도*를 붙혀서 출력한다. (단, 원본 데이터에 반영되지 않습니다.)
 select concat(concat('*',name),'*') 이름 from users;
 --26. 생년월일을 그대로 출력하지 말고
---    xx 년 xx 월 xx 일 형식으로 출력한다. (단, 원본 데이터에 반영되지 않습니다. 성별은 무시한다)
+--    xx 년  xx 월 xx 일 형식으로 출력한다. (단, 원본 데이터에 반영되지 않습니다. 성별은 무시한다)
 select '19'||concat(substr(jumin,1,2),'년')||' '||concat(substr(jumin,3,2),'월')||' '||concat(substr(jumin,5,2),'일') 생년월일 from users;
 --27. 이름, 포인트, 학년, 생년월일, 성별을 추가한다. 성별은 생년월일로 판단하며 마지막 숫자가1이면 남
 --    2이면 여라고 표시한다. 
